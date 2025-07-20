7e0423b1ea2ba840ee5ef6a890a0bd25c01d7c1f
Crizz <darynalsup@gmail.com>
2025-07-20 05:40:27 -0700

    Update view.py

--- cogs/admin_commands/view.py
+++ cogs/admin_commands/view.py
@@ -31,7 +31,7 @@ async def view_command(interaction: discord.Interaction):
     await interaction.followup.send(embed=embed, view=view, ephemeral=True)
 
 # Hook
-def start(admin_group: app_commands.Command, bot):
+async def start(admin_group: app_commands.Group, bot):
     command = app_commands.Command(
         name="view",
         description="View flagged users.",

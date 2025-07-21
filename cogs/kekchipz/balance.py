c0a107d3ee7814416efb3272723a7a5dd19b5238
Crizz <darynalsup@gmail.com>
2025-07-21 03:35:43 -0700

    Update balance.py - height

--- cogs/kekchipz/balance.py
+++ cogs/kekchipz/balance.py
@@ -96,7 +96,7 @@ async def create_kekchipz_balance_image(guild_id: int, discord_id: int, player_d
 
         # Center the text
         x = (base_image.width - text_width) // 2
-        y = (base_image.height - text_height) // 1.25
+        y = (base_image.height - text_height) // 4
 
         # Draw the text on the image
         draw.text((x, y), balance_text, font=font, fill=text_color)
